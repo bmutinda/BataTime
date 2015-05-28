6412088aacce926124ab8593479eec947af0c6eb
mutinda <boniface.info@gmail.com>
2015-05-28 17:10:47 +0300

    Added library code

---
+++ Library/BataTime.java
@@ -0,0 +1,67 @@
+package ke.co.zuku.zukuapp.libs.batatime;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Created by Mutinda Boniface on 5/15/2015.
+ */
+public class BataTime {
+    // Total Time the timer should take in milliseconds
+    public int time = 0;
+    // The tick interval
+    public int tick = 500;
+
+    private Timer timer;
+    private TickTimerTask timerTask;
+    public BataTimeCallback callback;
+
+    // Time elapsed since the timer started
+    private int elapsed = 0;
+
+    public BataTime(int time) {
+        this.create(time, this.tick);
+    }
+
+    public BataTime(int time, int tick) {
+        this.create(time, tick);
+    }
+
+    private void create(int time, int tick) {
+        this.time = time;
+        this.tick = tick;
+        timerTask = new TickTimerTask();
+    }
+
+    public void start( BataTimeCallback timerCallback ){
+        this.stop();
+
+        timer = new Timer();
+        timer.schedule(timerTask, 0, this.tick );
+        this.callback = timerCallback;
+    }
+
+    public void stop() {
+        if (timer != null) {
+            timer.cancel();
+        }
+    }
+
+    class TickTimerTask extends TimerTask {
+
+        @Override
+        public void run() {
+            if( elapsed>= time ){
+                stop( );
+                if( callback!=null ){
+                    callback.onComplete();
+                }
+            }else{
+                if( callback!=null ){
+                    callback.onUpdate(elapsed);
+                }
+                elapsed+=tick;
+            }
+        }
+    }
+}
---
+++ Library/BataTimeCallback.java
@@ -0,0 +1,9 @@
+package ke.co.zuku.zukuapp.libs.batatime;
+
+/**
+ * Created by Mutinda Boniface on 5/15/2015.
+ */
+public interface BataTimeCallback {
+    public void onUpdate( int elapsed );
+    public void onComplete( );
+}
